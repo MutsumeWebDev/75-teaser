1ad3142193badade153ea7f3e18c47ee89bf13c4
Y-As(HOP) <kulungu01@gmail.com>
2024-05-31 15:01:13 +0900

    student.html内の軽微な変更とページ内リンクの修正

--- scripts/menubar.js
+++ scripts/menubar.js
@@ -1,13 +1,13 @@
 const detail__list__top = '<ul class="detail__list__top">\
-<li class="lists"><a href="kyosan.html">協賛のお願い</a></li>\
-<li class="lists__last"><a href="student.html">参加団体へ</a></li>\
+<li class="lists"><a href="/kyosan.html">協賛のお願い</a></li>\
+<li class="lists__last"><a href="/student.html">参加団体へ</a></li>\
 <!-- <li class="lists"><a href="">テーマ発表</a></li>\
 <li class="lists__last"><a href="">公式テーマソング</a></li> --></ul>';
 
 const detail__list__bottom = '<ul class="detail__list__bottom">\
-<li class="lists__bottom"><a href="index.html">トップ</a></li>\
-<li class="lists__bottom"><a href="kyosan.html">協賛について</a></li>\
-<li class="lists__last__bottom"><a href="">参加団体へ</a></li>\
+<li class="lists__bottom"><a href="/index.html">トップ</a></li>\
+<li class="lists__bottom"><a href="/kyosan.html">協賛について</a></li>\
+<li class="lists__last__bottom"><a href="/student.html">参加団体へ</a></li>\
 <!-- <li class="lists__bottom"><a href="">テーマ</a></li>\
 <li class="lists__last__bottom"><a href="">公式テーマソング</a></li> --></ul>';
 

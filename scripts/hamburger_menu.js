75181710ad59b16ff84e686b7859a556616d1f20
Y-As(HOP) <kulungu01@gmail.com>
2024-06-09 21:50:27 +0900

    スマホ横画面だとメニューバーが見切れる Fixes #59

--- scripts/hamburger_menu.js
+++ scripts/hamburger_menu.js
@@ -1,9 +1,13 @@
 $(".openbtn").click(function () {
+    // active状態ならハンバーガーメニューの内容を表示
     $(this).toggleClass('active');
     $(".detail__list__top").toggleClass('active');
-});
-
-$("#g-nav a").click(function () {
-    $(".openbtn").removeClass('active');
-    $(".detail__list__top").removeClass('active');
+    // スクロールの対象をbodyとハンバーガーメニューで入れ替える
+    if ($("body").css("overflow") === "hidden"){
+        $("body").css({overflow: ""});
+        $(".detail__list__top").css({overflow: "hidden"});
+    } else {
+        $("body").css({overflow: "hidden"});
+        $(".detail__list__top").css({overflow: "scroll"});
+    }
 });

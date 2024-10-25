543c4efc84cef379cf783e2beb6cb9ad4104333a
hamanishi932 <136817555+hamanishi932@users.noreply.github.com>
2024-10-26 01:20:50 +0900

    公式webへ遷移

---
+++ scripts/alart.js
@@ -0,0 +1,4 @@
+// 「https://shiken-kouryaku-joho.com/へ遷移します。」のメッセージを出力
+alert('https://shiken-kouryaku-joho.com/へ遷移します。')
+//  window.location.hrefで画面遷移させます。
+window.location.href = 'https://shiken-kouryaku-joho.com/';
